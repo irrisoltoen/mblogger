85bbfc7332cb56901bec191ccab0861f78bb838e
Irrisolto En email = irrisoltoen@gmail.com <VISITOR@irrisolto.(none)>
2012-07-24 19:03:54 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mblogger::Application.config.secret_token = 'ee8601ecd7b8bf2665bd8163ae033d6a48909f8b2b571d7991161ed9a44acead798940137337a91820c6a4c851d8de334eec241d3269a51dc05c0c306b370ed6'
