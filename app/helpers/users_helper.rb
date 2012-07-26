3eb6dbea553d8b895f81852619a9fcdfe6a20b53
Irrisolto En email = irrisoltoen@gmail.com <visitor@irrisolto.(none)>
2012-07-26 12:51:54 +0530

    completed full sign in, sign out, index of users, defined admin user and enabled destroy method for admin this compeltes the full restful interface for users, besides: proper authentication and authorization has been handled

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,9 @@
 module UsersHelper
-	def gravatar_for(user)
+	def gravatar_for(user,args={})
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-		image_tag gravatar_url, alt: user.name, class: "gravatar"
+		args[:alt]=user.name
+		args[:class]="gravatar"
+		image_tag gravatar_url, args
 	end
 end
